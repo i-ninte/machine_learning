06c36ec3ad551013946a45b085faf635a858b955
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-05 18:54:09 +0000

    update time.py

---
+++ time.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import time
+size_of_vec= 1000
+X = np.arange(size_of_vec)
+Y= np.arange(size_of_vec)
+Z= X + Y
+def pure_python_version():
+	t1 = time.time()
+	
+	x = range(size_of_vec)
+	y = range(size_of_vec)
+	z = [X[i] + Y[i] for i in range(len(X))  ] 
+	return time.time() - t1
+
+
+
+def numpy_version():
+	t1= time.time()
+	
+	return time.time()
+
+
+t1= pure_python_version()
+t2= numpy_version()
+
+
+print(t1,t2)
+print("Numpy is in this example" + str(t1/t2) + " faster ")
