2164ad6f0b6576ab8433d8c70000e350d953f8bf
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-08 02:56:46 +0000

    upload numpy_02.py

---
+++ numpy_02.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=np.arange(1,10)
+print(a)
+x= range(1,10,2)
+#print(x)
+print(list(x))
+x= np.arange(10.4)
+print(x)
+x =np.arange(0.5,10.4,0.8)
+print(x)
+print (np.linspace(1,10))
+x=np.array(11)
+print(np.shape(x))
