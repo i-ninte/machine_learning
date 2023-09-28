7558792f22b68bd62b900a4e549fb003268f3729
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-09-28 16:09:57 +0000

    Update train_test_split.py

--- sklearn_models/train_test_split.py
+++ sklearn_models/train_test_split.py
@@ -1,1 +1,29 @@
 
+from sklearn import dataset
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+iris= dataset.load_iris() # loading dataset 
+
+
+#spitting dataset into features and labels
+
+X = iris.data
+y= iris.target
+print(X,y)
+
+# checking the number of enteries 
+print(X.shape)
+pint(y.shape)
+
+# splitting 
+X_train, X_test, y_train, y_test= train_test_split(X,y, test_size=0.3)
+# checking the dimensions of the test data
+
+print(X_train.shape)
+
+print(y_train.shape)
+
+# dimensions of test
+print(X_test.shape)
+print(y_test.shape)
