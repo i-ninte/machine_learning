470320e68f8c30ba09961e5e7b444c091a0adce2
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-08 19:36:16 +0000

    Update MLB.py

--- projects/MLB.py
+++ projects/MLB.py
@@ -3,3 +3,18 @@ def col_list_sum(df, col_list, weights=None):
     if weights is not None:
         col_df = col_df.multiply(weights)
     return col_df.sum(axis=1)
+
+df = pd.DataFrame({
+  'T1': [10, 15, 8],
+  'T2': [25, 27, 25],
+  'T3': [16, 15, 10]})
+  
+print('{}\n'.format(df))
+
+print('{}\n'.format(df.sum()))
+
+print('{}\n'.format(df.sum(axis=1)))
+
+print('{}\n'.format(df.mean()))
+
+print('{}\n'.format(df.mean(axis=1)))
