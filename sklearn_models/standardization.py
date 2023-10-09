b0f19d20bced729df1be55f1b34e327178880524
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-09 00:22:51 +0000

    Update standardization.py

--- sklearn_models/standardization.py
+++ sklearn_models/standardization.py
@@ -1,1 +1,16 @@
+# predefined pizza data
+# Newline to separate print statements
+print('{}\n'.format(repr(pizza_data)))
 
+from sklearn.preprocessing import scale
+# Standardizing each column of pizza_data
+col_standardized = scale(pizza_data)
+print('{}\n'.format(repr(col_standardized)))
+
+# Column means (rounded to nearest thousandth)
+col_means = col_standardized.mean(axis=0).round(decimals=3)
+print('{}\n'.format(repr(col_means)))
+
+# Column standard deviations
+col_stds = col_standardized.std(axis=0)
+print('{}\n'.format(repr(col_stds)))
