49e5f68d4ee0c69c34631a7395d3e830521e2705
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-10 08:21:45 +0000

    Update ridge_reg.py
    modelling using the RidgeCV for cross validation

--- sklearn_models/ridge_reg.py
+++ sklearn_models/ridge_reg.py
@@ -5,3 +5,12 @@ print('Coefficients: {}\n'.format(repr(reg.coef_)))
 print('Intercept: {}\n'.format(reg.intercept_))
 r2 = reg.score(pizza_data, pizza_prices)
 print('R2: {}\n'.format(r2))
+
+#adding a CV to select the alpha
+from sklearn import linear_model
+alphas = [0.1, 0.2, 0.3]
+reg = linear_model.RidgeCV(alphas=alphas)
+reg.fit(pizza_data, pizza_prices)
+print('Coefficients: {}\n'.format(repr(reg.coef_)))
+print('Intercept: {}\n'.format(reg.intercept_))
+print('Chosen alpha: {}\n'.format(reg.alpha_))
