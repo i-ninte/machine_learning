f513ed983726a70cc3c67a08defa4a04023cc92e
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-10 18:19:07 +0000

    Update bayesian_reg.py

--- sklearn_models/bayesian_reg.py
+++ sklearn_models/bayesian_reg.py
@@ -1,1 +1,12 @@
+# predefined dataset from previous chapter
+print('Data shape: {}\n'.format(data.shape))
+print('Labels shape: {}\n'.format(labels.shape))
 
+from sklearn import linear_model
+reg = linear_model.BayesianRidge()
+reg.fit(data, labels)
+print('Coefficients: {}\n'.format(repr(reg.coef_)))
+print('Intercept: {}\n'.format(reg.intercept_))
+print('R2: {}\n'.format(reg.score(data, labels)))
+print('Alpha: {}\n'.format(reg.alpha_))
+print('Lambda: {}\n'.format(reg.lambda_))
