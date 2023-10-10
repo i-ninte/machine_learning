b2dd122c89807aaaa094089d8ea72b056119461e
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-10 08:43:22 +0000

    Update LASSO_reg.py

--- sklearn_models/LASSO_reg.py
+++ sklearn_models/LASSO_reg.py
@@ -8,3 +8,9 @@ reg.fit(data, labels)
 print('Coefficients: {}\n'.format(repr(reg.coef_)))
 print('Intercept: {}\n'.format(reg.intercept_))
 print('R2: {}\n'.format(reg.score(data, labels)))
+
+
+def lasso_reg(data, labels, alpha):
+  reg=linear_model.Lasso(alpha)
+  reg.fit(data, labels)
+  return reg
