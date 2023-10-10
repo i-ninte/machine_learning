fb2af805f0ac4b1d442a798ba35b22473b387776
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-10 08:18:46 +0000

    Update ridge_reg.py

--- sklearn_models/ridge_reg.py
+++ sklearn_models/ridge_reg.py
@@ -1,1 +1,7 @@
-
+from sklearn import linear_model
+reg = linear_model.Ridge(alpha=0.1)
+reg.fit(pizza_data, pizza_prices)
+print('Coefficients: {}\n'.format(repr(reg.coef_)))
+print('Intercept: {}\n'.format(reg.intercept_))
+r2 = reg.score(pizza_data, pizza_prices)
+print('R2: {}\n'.format(r2))
