b1799912db6fd951ae2e41bf75c35038827f69c6
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-10 08:38:58 +0000

    Update LASSO_reg.py

--- sklearn_models/LASSO_reg.py
+++ sklearn_models/LASSO_reg.py
@@ -1,1 +1,10 @@
+# predefined dataset
+print('Data shape: {}\n'.format(data.shape))
+print('Labels shape: {}\n'.format(labels.shape))
 
+from sklearn import linear_model
+reg = linear_model.Lasso(alpha=0.1)
+reg.fit(data, labels)
+print('Coefficients: {}\n'.format(repr(reg.coef_)))
+print('Intercept: {}\n'.format(reg.intercept_))
+print('R2: {}\n'.format(reg.score(data, labels)))
