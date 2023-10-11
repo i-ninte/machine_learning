8d95b2481c40807471aee420d63a48d517f209f3
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-11 03:49:36 +0000

    Update logistic_reg.py
    multinomial strategy with solvers and max_iter

--- sklearn_models/logistic_reg.py
+++ sklearn_models/logistic_reg.py
@@ -1,1 +1,36 @@
 
+
+
+# predefined dataset
+print('Data shape: {}\n'.format(data.shape))
+# Binary labels
+print('Labels:\n{}\n'.format(repr(labels)))
+
+from sklearn import linear_model
+reg = linear_model.LogisticRegression()
+reg.fit(data, labels)
+
+new_data = np.array([
+  [  0.3,  0.5, -1.2,  1.4],
+  [ -1.3,  1.8, -0.6, -8.2]])
+print('Prediction classes: {}\n'.format(
+  repr(reg.predict(new_data))))
+
+
+#multinomial strategy implementation with lbfgs solver
+# predefined dataset
+print('Data shape: {}\n'.format(data.shape))
+# Multiclass labels
+print('Labels:\n{}\n'.format(repr(labels)))
+
+from sklearn import linear_model
+reg = linear_model.LogisticRegression(
+  solver='lbfgs',
+  multi_class='multinomial', max_iter=200)
+reg.fit(data, labels)
+
+new_data = np.array([
+  [ 1.8, -0.5, 6.2, 1.4],
+  [ 3.3,  0.8, 0.1, 2.5]])
+print('Prediction classes: {}\n'.format(
+  repr(reg.predict(new_data))))
