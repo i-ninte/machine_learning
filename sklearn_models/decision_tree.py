2391e47e11ec11ccb3bacd78e6890e0249cff6af
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-12 02:53:11 +0000

    Update decision_tree.py

--- sklearn_models/decision_tree.py
+++ sklearn_models/decision_tree.py
@@ -11,3 +11,15 @@ print('Data shape: {}\n'.format(data.shape))
 # Binary labels
 print('Labels:\n{}\n'.format(repr(labels)))
 clf_tree1.fit(data, labels)
+
+
+#applying cv to decision tree to determine the best max_depth
+is_clf = True  # for classification
+for depth in range(3, 8):
+  # Predefined data and labels
+  scores = cv_decision_tree(
+    is_clf, data, labels, depth, 5)  # k = 5
+  mean = scores.mean()  # Mean acc across folds
+  std_2 = 2 * scores.std()  # 2 std devs
+  print('95% C.I. for depth {}: {} +/- {:.2f}\n'.format(
+    depth, mean, std_2))
