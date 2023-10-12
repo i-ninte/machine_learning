a15f432aef8db696af5465b45fc2aa2bad0ee95e
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-12 01:29:46 +0000

    Update decision_tree.py

--- sklearn_models/decision_tree.py
+++ sklearn_models/decision_tree.py
@@ -1,1 +1,13 @@
+from sklearn import tree
+clf_tree1 = tree.DecisionTreeClassifier()
+reg_tree1 = tree.DecisionTreeRegressor()
+clf_tree2 = tree.DecisionTreeClassifier(
+  max_depth=8)  # max depth of 8
+reg_tree2 = tree.DecisionTreeRegressor(
+  max_depth=5)  # max depth of 5
 
+# predefined dataset
+print('Data shape: {}\n'.format(data.shape))
+# Binary labels
+print('Labels:\n{}\n'.format(repr(labels)))
+clf_tree1.fit(data, labels)
