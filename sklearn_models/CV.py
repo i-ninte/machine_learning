6c0c7d6787e549f5e944f0d8c9289049e31ddbbe
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-12 02:37:24 +0000

    Update CV.py

--- sklearn_models/CV.py
+++ sklearn_models/CV.py
@@ -1,1 +1,10 @@
+from sklearn import linear_model
+from sklearn.model_selection import cross_val_score
+# We can skip max_iter argument here, but it will produce a
+# ConvergenceWarning. Therefore we explicity give a bigger value to
+# avoid the warning.
+clf = linear_model.LogisticRegression(max_iter=3000)
+# Predefined data and labels
+cv_score = cross_val_score(clf, data, labels, cv=3)  # k = 3
 
+print('{}\n'.format(repr(cv_score)))
