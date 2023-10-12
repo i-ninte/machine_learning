f58cca702161f0ca7458e77b63214e3a46ada087
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-12 02:16:40 +0000

    Update train_test_split.py

--- sklearn_models/train_test_split.py
+++ sklearn_models/train_test_split.py
@@ -24,6 +24,33 @@ print(X_train.shape)
 
 print(y_train.shape)
 
+
+#educative
 # dimensions of test
 print(X_test.shape)
 print(y_test.shape)
+
+
+data = np.array([
+  [10.2 ,  0.5 ],
+  [ 8.7 ,  0.9 ],
+  [ 9.3 ,  0.8 ],
+  [10.1 ,  0.4 ],
+  [ 9.5 ,  0.77],
+  [ 9.1 ,  0.68],
+  [ 7.7 ,  0.9 ],
+  [ 8.3 ,  0.8 ]])
+labels = np.array(
+  [1.4, 1.2, 1.6, 1.5, 1.6, 1.3, 1.1, 1.2])
+
+from sklearn.model_selection import train_test_split
+split_dataset = train_test_split(data, labels)
+train_data = split_dataset[0]
+test_data = split_dataset[1]
+train_labels = split_dataset[2]
+test_labels = split_dataset[3]
+
+print('{}\n'.format(repr(train_data)))
+print('{}\n'.format(repr(train_labels)))
+print('{}\n'.format(repr(test_data)))
+print('{}\n'.format(repr(test_labels)))
