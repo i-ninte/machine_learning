085abbc67bcadf4f46aef70e958ed6f735827dc0
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-13 04:56:44 +0000

    Update mini_batch_kmeans.py

--- sklearn_models/mini_batch_kmeans.py
+++ sklearn_models/mini_batch_kmeans.py
@@ -1,1 +1,16 @@
+from sklearn.cluster import MiniBatchKMeans
+kmeans = MiniBatchKMeans(n_clusters=3, batch_size=10)
+# predefined data
+kmeans.fit(data)
 
+# cluster assignments
+print('{}\n'.format(repr(kmeans.labels_)))
+
+# centroids
+print('{}\n'.format(repr(kmeans.cluster_centers_)))
+
+new_obs = np.array([
+  [5.1, 3.2, 1.7, 1.9],
+  [6.9, 3.2, 5.3, 2.2]])
+# predict clusters
+print('{}\n'.format(repr(kmeans.predict(new_obs))))
