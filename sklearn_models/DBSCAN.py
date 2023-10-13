7ed6dfb70d7156ac7ce145bd79aa5af225549a31
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-13 05:47:04 +0000

    Update DBSCAN.py

--- sklearn_models/DBSCAN.py
+++ sklearn_models/DBSCAN.py
@@ -1,1 +1,12 @@
+from sklearn.cluster import DBSCAN
+dbscan = DBSCAN(eps=1.2, min_samples=30)
+# predefined data
+dbscan.fit(data)
 
+# cluster assignments
+print('{}\n'.format(repr(dbscan.labels_)))
+
+# core samples
+print('{}\n'.format(repr(dbscan.core_sample_indices_)))
+num_core_samples = len(dbscan.core_sample_indices_)
+print('Num core samples: {}\n'.format(num_core_samples))
