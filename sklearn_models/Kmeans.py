6138b1258b3ef4b20de7abe432c8f2170b6d2abe
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-13 04:50:23 +0000

    Update Kmeans.py

--- sklearn_models/Kmeans.py
+++ sklearn_models/Kmeans.py
@@ -1,43 +1,35 @@
-from sklearn.datasets import lod_breast_cancer
-from sklearn.cluster import Kmeans
-from sklearn.model_selection import train_test_split 
-from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_breast_cancer
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, silhouette_score, adjusted_rand_score
 from sklearn.preprocessing import scale
-import pandaas as pd
+import pandas as pd
 
-bc=load_breast_cancer()
-print(bc)
+def bench_k_means(estimator, name, data, labels):
+    estimator.fit(data)
+    print(f'{name}:')
+    print(f'Silhouette Score: {silhouette_score(data, estimator.labels_)}')
+    print(f'Adjusted Rand Index: {adjusted_rand_score(labels, estimator.labels_)}')
 
-X=bc.data
-#scaling the model
-X=scale(bc.data)
-print (X)
+bc = load_breast_cancer()
+X = bc.data
+X = scale(bc.data)
+y = bc.target
 
-y= bc.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-
-X_train,X_test, y_train,y_test = train_test_split(X,y,test_size=0.2)
-
-model= Kmeans(n_clusters=2, random_state=0)
-
+model = KMeans(n_clusters=2, random_state=0)
 model.fit(X_train)
+predictions = model.predict(X_test)
+labels = model.labels
 
+print('Labels:', labels)
+print('Predictions:', predictions)
+print('Accuracy:', accuracy_score(y_test, predictions))
+print('Actual:', y_test)
 
-predictions= model.pedict(x_test)
-
-labels=model.labels
-print('labels',labels)
-print('predictions', predictions)
-print('accuracy', accuracy_score(y_test, predictions))
-print('actual', y_test)
-
-#cross tabulation test
+# Cross-tabulation test
 print(pd.crosstab(y_train, labels))
 
-
-#sklean checking the metrics 
-def bench_k_means(estimator, name, data):
-	estimator.fit(data)
-	print()
-
-bench_k_means(X,y)
+# Benchmark K-Means
+bench_k_means(model, 'K-Means', X_train, y_train)
