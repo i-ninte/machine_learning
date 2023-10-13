b529e9f5d0f9d13b059dc7c7a46fb2c4244ddc77
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-13 05:58:12 +0000

    Update AMI.py

--- sklearn_models/AMI.py
+++ sklearn_models/AMI.py
@@ -1,1 +1,31 @@
+from sklearn.metrics import adjusted_mutual_info_score
+true_labels = np.array([0, 0, 0, 1, 1, 1])
+pred_labels = np.array([0, 0, 1, 1, 2, 2])
 
+ami = adjusted_mutual_info_score(true_labels, pred_labels)
+print('{}\n'.format(ami))
+
+# symmetric
+ami = adjusted_mutual_info_score(pred_labels, true_labels)
+print('{}\n'.format(ami))
+
+# Perfect labeling
+perf_labels = np.array([0, 0, 0, 1, 1, 1])
+ami = adjusted_mutual_info_score(true_labels, perf_labels)
+print('{}\n'.format(ami))
+
+# Perfect labeling, permuted
+permuted_labels = np.array([1, 1, 1, 0, 0, 0])
+ami = adjusted_mutual_info_score(true_labels, permuted_labels)
+print('{}\n'.format(ami))
+
+renamed_labels = np.array([1, 1, 1, 3, 3, 3])
+# Renamed labels to 1, 3
+ami = adjusted_mutual_info_score(true_labels, renamed_labels)
+print('{}\n'.format(ami))
+
+true_labels2 = np.array([0, 1, 2, 0, 3, 4, 5, 1])
+# Bad labeling
+pred_labels2 = np.array([1, 1, 0, 0, 2, 2, 2, 2])
+ami = adjusted_mutual_info_score(true_labels2, pred_labels2)
+print('{}\n'.format(ami))
