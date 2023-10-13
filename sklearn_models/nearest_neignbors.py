2c41758f0874d2dc9408a4f4a4486a3d0b900f9d
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-13 04:42:07 +0000

    Update nearest_neignbors.py

--- sklearn_models/nearest_neignbors.py
+++ sklearn_models/nearest_neignbors.py
@@ -1,1 +1,26 @@
+data = np.array([
+  [5.1, 3.5, 1.4, 0.2],
+  [4.9, 3. , 1.4, 0.2],
+  [4.7, 3.2, 1.3, 0.2],
+  [4.6, 3.1, 1.5, 0.2],
+  [5. , 3.6, 1.4, 0.2],
+  [5.4, 3.9, 1.7, 0.4],
+  [4.6, 3.4, 1.4, 0.3],
+  [5. , 3.4, 1.5, 0.2],
+  [4.4, 2.9, 1.4, 0.2],
+  [4.9, 3.1, 1.5, 0.1]])
 
+from sklearn.neighbors import NearestNeighbors
+nbrs = NearestNeighbors()
+nbrs.fit(data)
+new_obs = np.array([[5. , 3.5, 1.6, 0.3]])
+dists, knbrs = nbrs.kneighbors(new_obs)
+
+# nearest neighbors indexes
+print('{}\n'.format(repr(knbrs)))
+# nearest neighbor distances
+print('{}\n'.format(repr(dists)))
+
+only_nbrs = nbrs.kneighbors(new_obs,
+                            return_distance=False)
+print('{}\n'.format(repr(only_nbrs)))
