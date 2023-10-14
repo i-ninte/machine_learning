6de879c48d1c51580c5fd57c726f3a5a1db5db2f
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-14 07:29:52 +0000

    Update AMI.py

--- sklearn_models/AMI.py
+++ sklearn_models/AMI.py
@@ -29,3 +29,4 @@ true_labels2 = np.array([0, 1, 2, 0, 3, 4, 5, 1])
 pred_labels2 = np.array([1, 1, 0, 0, 2, 2, 2, 2])
 ami = adjusted_mutual_info_score(true_labels2, pred_labels2)
 print('{}\n'.format(ami))
+
