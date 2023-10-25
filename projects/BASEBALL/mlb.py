a812b72300e2d1e7defa836be109e6e756f8bea8
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-25 13:07:47 +0000

    Create mlb.py

---
+++ projects/BASEBALL/mlb.py
@@ -0,0 +1,16 @@
+def col_list_sum(df, col_list, weights=None):
+    col_df = df[col_list]
+    if weights is not None:
+        col_df = col_df.multiply(weights)
+    return col_df.sum(axis=1)
+
+
+mlb_df['BA']=mlb_df['H']/mlb_df['AB']
+
+other_hits= col_list_sum(mlb_df,['2B','3B','HR'])
+mlb_df['1B']= mlb_df['H'] - other_hits
+
+weighted_hits = col_list_sum(mlb_df, ['1B', '2B', '3B', 'HR'],weights=[1, 2, 3, 4])
+
+mlb_df['SLG']= weighted_hits/mlb_df['AB']
+
