457ada679795c0a196c4004f2dc43abf21d54429
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-10-31 16:21:33 +0000

    Update xlsx_to_csv.py

--- projects/xlsx_to_csv.py
+++ projects/xlsx_to_csv.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+
+input_file = 'mbpp.xlsx'
+output_file = 'mbpp.csv'
+
+# Read the XLSX file into a DataFrame.
+df = pd.read_excel(input_file)
+
+# Convert the DataFrame to a CSV file.
+df.to_csv(output_file, index=False)
+
+print(f'Conversion from {input_file} to {output_file} complete.')
 
