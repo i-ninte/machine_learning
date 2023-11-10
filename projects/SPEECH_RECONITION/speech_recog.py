e75a0c0efb8a8d9f0a226b02887fa517a1f28d35
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-11-10 13:21:12 +0000

    Update speech_recog.py

--- projects/SPEECH_RECONITION/speech_recog.py
+++ projects/SPEECH_RECONITION/speech_recog.py
@@ -1,1 +1,14 @@
+sr= speech_recognition.Recognizer()
 
+
+with speech_recognition.Microphone() as source2:
+
+
+    print("Silence please ")
+    sr.adjust_for_ambient_noise(source2, duration =2)
+    print(" Speak now please .....")
+    audio2= sr.listen(source2)
+    textt= sr.recognize_google(audio2)
+    textt= textt.lower()
+    print("Did you say : "+ textt)
+    
